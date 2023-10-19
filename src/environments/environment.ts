bf317a64db5a8a49e1e12065f042f60d3b367d90
DangDAO1997 <daoanhdangd15ptit@gmail.com>
2023-10-19 23:11:02 +0700

    update enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://acfe335eb4f564ae4ba79c5c9335bf38-1434815378.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
